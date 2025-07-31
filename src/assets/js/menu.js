2f47236e70574cd5364113ecb802ad901255fcb8
no2reum <no2reum@naver.com>
2025-07-31 11:32:10 +0900

    😘 헤더 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,14 @@
 export function menu() {
-    console.log(menu)
+    const headerToggle = document.getElementById("headerToggle"); /* getelementid-철자틀려서 js가 제대로 작동하지 않았음(헤더만들기 중) */
+    const headerNav = document.querySelector(".header__nav");
+
+    if(headerToggle){
+        headerToggle.addEventListener("click", () => {
+        headerNav.classList.toggle("show");
+
+       headerToggle.getAttribute("aria-expanded")  === "true" 
+       ? headerToggle.setAttribute("aria-expanded", "false") 
+       : headerToggle.setAttribute("aria-expanded", "true")
+    });
+    }
 }
